593d2ea9bfe8d85c4935736c27552ad437aa20eb
Alex Chen <alex@onekingslane.com>
2013-04-09 15:16:46 -0700

    last

--- checker_board.py
+++ checker_board.py
@@ -5,12 +5,13 @@ import matplotlib.cm as cm
 #import matplotlib.image as MPI
 
 class CheckerBoard:
-    def __init__(self, r_pixel=800, c_pixel=800, num_block=8):
+    def __init__(self, r_pixel=800, c_pixel=800, num_block=4):
+        
         self.r_pixel = r_pixel
         self.c_pixel = c_pixel
         self.num_block = num_block
-        self.r_block = self.r_pixel / num_block
-        self.c_block = self.c_pixel / num_block
+        self.r_block = self.r_pixel / self.num_block
+        self.c_block = self.c_pixel / self.num_block
         
                         
     def generate_pixels1(self):
@@ -22,9 +23,9 @@ class CheckerBoard:
                     self.pixels[j*self.c_block:(j+1)*self.c_block:1, i*self.r_block:(i+1)*self.r_block:1] = 1
                     
     def generate_pixels2(self):
-        a = NP.fromfunction(lambda i, j: i % 200 < 100, (800, 800), dtype=int)
-        b = NP.fromfunction(lambda i, j: j % 200 < 100, (800, 800), dtype=int)
-        self.pixels = NP.zeros((self.c_pixel, self.r_pixel), dtype='bool') - a - b         
+        a = NP.fromfunction(lambda i, j: i % self.c_block * 2 < self.c_block, (self.c_pixel, self.r_pixel), dtype=int)
+        b = NP.fromfunction(lambda i, j: j % self.r_block * 2 < self.r_block, (self.c_pixel, self.r_pixel), dtype=int)        
+        self.pixels = NP.zeros((self.c_pixel, self.r_pixel), dtype='bool') - a - b
                                 
     def render(self):
         MPL.imshow(self.pixels, cmap = cm.gist_gray)
