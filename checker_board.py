8e64d7427cdbb2b04645585f2b2b032a51fa57f0
Alex Chen <alex@onekingslane.com>
2013-04-10 13:52:34 -0700

    method2

--- checker_board.py
+++ checker_board.py
@@ -5,11 +5,11 @@ import matplotlib.cm as cm
 #import matplotlib.image as MPI
 
 class CheckerBoard:
-    def __init__(self, r_pixel=800, c_pixel=800, num_block=4):
-        
+    def __init__(self, r_pixel=800, c_pixel=800, num_block=8):        
         self.r_pixel = r_pixel
         self.c_pixel = c_pixel
         self.num_block = num_block
+        
         self.r_block = self.r_pixel / self.num_block
         self.c_block = self.c_pixel / self.num_block
         
@@ -25,13 +25,28 @@ class CheckerBoard:
     def generate_pixels2(self):
         a = NP.fromfunction(lambda i, j: i % self.c_block * 2 < self.c_block, (self.c_pixel, self.r_pixel), dtype=int)
         b = NP.fromfunction(lambda i, j: j % self.r_block * 2 < self.r_block, (self.c_pixel, self.r_pixel), dtype=int)        
-        self.pixels = NP.zeros((self.c_pixel, self.r_pixel), dtype='bool') - a - b
-                                
+        self.pixels = a-b
+                    
+    def generate_pixels3(self):
+        black_block = NP.zeros((self.c_block, self.r_block))
+        white_block = NP.ones((self.c_block, self.r_block))
+        two_block1 = NP.hstack((black_block, white_block))
+        two_block2 = NP.hstack((white_block, black_block))        
+        four_block = NP.vstack((two_block1, two_block2))
+        self.pixels = NP.tile(four_block, (self.num_block/2, self.num_block/2))
+        
+    def generate_pixels4(self):
+        c = NP.arange(0, self.c_pixel, 1)
+        r = NP.arange(0, self.r_pixel, 1)
+        C, R = meshgrid(c, r)        
+        self.pixels = NP.remainder(C % 200 < 100)
+        self.pixels = NP.remainder(R % 200 < 100)
+                                         
     def render(self):
         MPL.imshow(self.pixels, cmap = cm.gist_gray)
         MPL.show()
         
   
 cb = CheckerBoard()
-cb.generate_pixels2()
+cb.generate_pixels3()
 cb.render()
