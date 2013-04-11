c44f1a173ac11d27c0b1ed905b95fe42ba7295b4
Alex Chen <alex@onekingslane.com>
2013-04-10 18:23:03 -0700

    even cleaner code

--- run_length_encoder.py
+++ run_length_encoder.py
@@ -1,9 +1,8 @@
 import numpy as NP
 
 #generate a random int array
-x = NP.random.randint(0,3,100)
+x = NP.random.randint(0,3,10)
 print str(x)+' is encoded to'
-run_starts, = NP.where(NP.diff(NP.hstack(([0], x, [0]))) != 0)
-print [x[run_starts[i]] if (run_starts[i+1] == run_starts[i]+1) else (x[run_starts[i]], run_starts[i+1]-run_starts[i]) for i in range(len(run_starts)-1)]
-
-
+run_starts, = NP.where(NP.diff(NP.hstack(([-100], x, [-100]))) != 0)
+encoded = [x[s] if (e==s) else (x[s], e-s+1) for s, e in zip(run_starts[:-1], NP.roll(run_starts-1, -1)[:-1])]
+print encoded
