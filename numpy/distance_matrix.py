95f5a8d736924f6c8ed4fd6071dae261c963902b
Alex Chen <alexchen@Alexs-MacBook-Air.local>
2013-11-05 23:23:49 -0800

    dm

--- numpy/distance_matrix.py
+++ numpy/distance_matrix.py
@@ -1,9 +1,14 @@
 import numpy as NP 
 
 def distance_matrix(matrix):
-	
+  r, c = matrix.shape
+  return NP.sqrt(NP.sum((matrix.reshape(1, r, c) - matrix.reshape(r, 1, c))**2, axis=2))
 
+r=1000
+c=100
+matrix = NP.random.randint(0, 10, r*c).reshape(r, c)
+print 'matrix', matrix.shape, matrix.nbytes/1000000
 
-matrix = NP.random.randint(0, 10, 12).reshape(3, 4)
 dm = distance_matrix(matrix)
+print 'dm:', dm.shape, dm.nbytes/1000000
 print dm
